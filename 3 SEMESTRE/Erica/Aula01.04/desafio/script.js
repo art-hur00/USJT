d1aac480243fd4a9f7ada0710ffd5be94e195b9c
Arthur <art.hur.alvesfarias00@gmail.com>
2025-04-02 20:20:33 -0300

    Desafio concluido

--- 3 SEMESTRE/Erica/Aula01.04/desafio/script.js
+++ 3 SEMESTRE/Erica/Aula01.04/desafio/script.js
@@ -1,11 +1,26 @@
 
 function exibirMensagemDeSucesso() {
+   
     const form = document.getElementById("botãoDeEnviar");
     const rspMen = document.getElementById("rspMen");
+  
     form.addEventListener('submit', function(event) {
         event.preventDefault();
         rspMen.style.display = 'block';
+        exibirCarregamento();
     });
 }
 
-
+function exibirCarregamento() {
+        const progressBar = document.getElementById('carregamento');
+        let progresso = 0;
+    
+    const interval = setInterval(() => {
+        if (progresso >= 100) {
+            clearInterval(interval);
+        } else {
+            progresso += 10;
+            progressBar.style.width = progresso + '%';
+        }
+    }, 500);
+}
